c059659a8d1ab0b2cdf926c8ebb7f42929155b25
Swagat Kulkarni <swagk@amazon.com>
2023-04-03 19:54:55 -0500

    Added exception type

--- src/main/java/ro/edu/aws/sgm/inference/pmml/randomforest/exception/InsufficientMemoryException.java
+++ src/main/java/ro/edu/aws/sgm/inference/pmml/randomforest/exception/InsufficientMemoryException.java
@@ -1,7 +1,11 @@
 package ro.edu.aws.sgm.inference.pmml.randomforest.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 public class InsufficientMemoryException extends RuntimeException {
 
+    @ResponseStatus(HttpStatus.INSUFFICIENT_STORAGE)
     public InsufficientMemoryException(String exception){
         super(exception);
     }
